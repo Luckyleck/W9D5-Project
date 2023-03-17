a0988f15a5b96c0bbf7ee765b1da83e62f07b540
smuushi <mishih780@gmail.com>
2023-03-17 16:56:10 -0400

    asdf

--- art-museum/src/components/GalleryNavigation/Gallery.jsx
+++ art-museum/src/components/GalleryNavigation/Gallery.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom"
 
 const Gallery = (prop) => {
+    // console.log(prop)
     console.log(prop)
     return (
-        <NavLink key={prop.key} to={`/galleries/${prop.galleryId}`}>{prop.galleryName}</NavLink>
+        <li>
+            <NavLink to={`/galleries/${prop.galleryId}`}>{prop.galleryName}</NavLink>
+        </li>
     )
 }
 
--- art-museum/src/components/GalleryNavigation/index.jsx
+++ art-museum/src/components/GalleryNavigation/index.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import Gallery from "./Gallery";
+import './GalleryNavigation.css';
 
 function GalleryNavigation(props) {
     // console.log(props)
 
     const {galleries} = props; // galleries comes from prop
-    console.log(galleries)
+    // console.log(galleries)
 
     const galleryLinks = galleries.map((gallery) => {
-        return <Gallery key={gallery.galleryNumber} galleryId={gallery.gallerynumber} galleryName={gallery.name}/>
+        return <Gallery key={gallery.gallerynumber} galleryId={gallery.gallerynumber} galleryName={gallery.name}/>
     })
 
     return (
         <nav>
             <h1>Galleries</h1>
-            <NavLink to="/">
+            <NavLink to="/" key="asdfasf">
                 index Link
             </NavLink>
             {galleryLinks}
