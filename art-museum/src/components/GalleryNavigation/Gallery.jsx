6e0e82b4b57ada884357356a52bcf1fcc48326b9
luckyleck <luckyleck29@gmail.com>
2023-03-17 16:02:14 -0400

    Gallery

---
+++ art-museum/src/components/GalleryNavigation/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Gallery = (prop) => {
+    console.log(prop)
+    return (
+        <h1>Hello from Gallery.jsx</h1>
+    )
+}
+
+export default Gallery;
--- art-museum/src/components/GalleryNavigation/index.jsx
+++ art-museum/src/components/GalleryNavigation/index.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import Gallery from "./Gallery";
 
 function GalleryNavigation(props) {
     // console.log(props)
 
     const {galleries} = props;
-
-
-
     console.log(galleries)
+
+    const galleryLinks = galleries.map((gallery) => {
+        return <Gallery galleryId={gallery.gallerynumber}/>
+    })
 
     return (
         <nav>
             <h1>Galleries</h1>
-            <NavLink to="/"></NavLink>
+            <NavLink to="/">
+                index Link
+            </NavLink>
+            {galleryLinks}
         </nav>
     );
 }
